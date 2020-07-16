21005e03e6b2ad4dd4b69a501312254e0c593fac
Charlie McHugh <cpmchugh1@gmail.com>
2020-07-16 03:17:49 +0100

    Moves for pawns, tests for others

--- chess_logic.py
+++ chess_logic.py
@@ -65,14 +65,13 @@ class ChessPiece(ABC):
     def __init__(self, position, team_colour):
         self.colour = team_colour
         self._position = position
-        pass
 
     @abstractmethod
     def get_letter_representation(self):
         pass
 
     @abstractmethod
-    def get_moves(self, board):
+    def get_moves(self, board, **kwargs):
         """
         Uses position to get all available squares the piece can move to.
 
@@ -94,11 +93,14 @@ class ChessPiece(ABC):
         board_tiles = board.get_tiles()
         return self.__get_moves_from_vectors(board_tiles, orthogonal_vectors)
 
+    def _get_cell_from_vector(self, cell, vector):
+        return cell[0] + vector[0], cell[1] + vector[1]
+
     def __get_moves_from_vectors(self, board_tiles, vectors):
         available_moves = []
         for vector in vectors:
-            curr_cell = self.__step(self._position, vector)
-            while self.__is_cell_on_board(curr_cell):
+            curr_cell = self._get_cell_from_vector(self._position, vector)
+            while self._is_cell_on_board(curr_cell):
                 x = curr_cell[0]
                 y = curr_cell[1]
                 if board_tiles[x][y] is not None:  # Piece in this position
@@ -110,13 +112,10 @@ class ChessPiece(ABC):
                         available_moves.append(curr_cell)
                         break
                 available_moves.append(curr_cell)
-                curr_cell = self.__step(curr_cell, vector)
+                curr_cell = self._get_cell_from_vector(curr_cell, vector)
         return available_moves
 
-    def __step(self, cell, vector):
-        return cell[0] + vector[0], cell[1] + vector[1]
-
-    def __is_cell_on_board(self, cell):
+    def _is_cell_on_board(self, cell):
         return 0 <= cell[0] <= 7 and 0 <= cell[1] <= 7
 
 
@@ -126,7 +125,7 @@ class King(ChessPiece):
         super().__init__(position, team_colour)
         self.is_in_check = False
 
-    def get_moves(self, board: ChessBoard):
+    def get_moves(self, board, **kwargs):
         pass
 
     def get_letter_representation(self):
@@ -134,7 +133,7 @@ class King(ChessPiece):
 
 
 class Queen(ChessPiece):
-    def get_moves(self, board: ChessBoard):
+    def get_moves(self, board, **kwargs):
         diagonal_moves = self._get_diagonal_moves(board)
         orthogonal_moves = self._get_orthogonal_moves(board)
         return orthogonal_moves + diagonal_moves
@@ -144,7 +143,7 @@ class Queen(ChessPiece):
 
 
 class Rook(ChessPiece):
-    def get_moves(self, board: ChessBoard):
+    def get_moves(self, board, **kwargs):
         return self._get_orthogonal_moves(board)
         pass
 
@@ -153,7 +152,7 @@ class Rook(ChessPiece):
 
 
 class Bishop(ChessPiece):
-    def get_moves(self, board: ChessBoard):
+    def get_moves(self, board, **kwargs):
         return self._get_diagonal_moves(board)
 
     def get_letter_representation(self):
@@ -161,17 +160,95 @@ class Bishop(ChessPiece):
 
 
 class Knight(ChessPiece):
-    def get_moves(self, board: ChessBoard):
+    def get_moves(self, board, **kwargs):
+        available_moves = []
+        knight_vectors = \
+            [(1, 2), (-1, 2), (1, -2), (-1, -2),
+                (2, 1), (2, -1), (-2, 1), (-2, -1)]
 
-        pass
+        board_tiles = board.get_tiles()
+        potential_moves = [
+            self._get_cell_from_vector(self._position, v)
+            for v in knight_vectors]
+        for move in potential_moves:
+            if not self._is_cell_on_board(move):
+                continue
+            board_tile = board_tiles[move[0]][move[1]]
+            if board_tile is not None and board_tile.colour is self.colour:
+                # Can't move to this tile if occupied by one of your pieces
+                continue
+            available_moves.append(move)
+
+        return available_moves
 
     def get_letter_representation(self):
         return 'N'
 
 
 class Pawn(ChessPiece):
-    def get_moves(self, board: ChessBoard):
-        pass
+    def __init__(self, position, team_colour):
+        super().__init__(position, team_colour)
+        self.__dir = 1 if team_colour == TeamColour.WHITE else -1
+
+    def get_moves(self, board, **kwargs):
+        if kwargs['prev_move'] is None:
+            raise Exception()
+
+        avail_moves = []
+        board_tiles = board.get_tiles()
+        curr_x, curr_y = self._position[0], self._position[1]
+
+        avail_moves += self.__get_l_r_moves(board_tiles, curr_x, curr_y)
+        avail_moves += self.__get_forward_moves(board_tiles, curr_x, curr_y)
+        avail_moves += self.__get_en_passant_moves(
+            board_tiles, curr_x, curr_y, kwargs['prev_move'])
+        return avail_moves
 
     def get_letter_representation(self):
         return 'P'
+
+    def __get_l_r_moves(self, board_tiles, curr_x, curr_y):
+        avail_moves = []
+        if self.colour == TeamColour.WHITE:
+            potential_moves = [(curr_x - 1, curr_y + 1),
+                (curr_x + 1, curr_y + 1)]
+        else:
+            potential_moves = [(curr_x - 1, curr_y - 1),
+                (curr_x + 1, curr_y - 1)]
+
+        for move in potential_moves:
+            tile = board_tiles[move[0]][move[1]]
+            if tile is not None and tile.colour is not self.colour:
+                avail_moves.append(move)
+
+        return avail_moves
+
+    def __get_forward_moves(self, board_tiles, curr_x, curr_y):
+        avail_moves = []
+        if self.colour == TeamColour.WHITE:
+            if board_tiles[curr_x][curr_y + 1] is None:
+                avail_moves.append((curr_x, curr_y + 1))
+            if curr_y == 1 and board_tiles[curr_x][curr_y + 2] is None:
+                avail_moves.append((curr_x, curr_y + 2))
+        else:
+            if board_tiles[curr_x][curr_y - 1] is None:
+                avail_moves.append((curr_x, curr_y - 1))
+            if curr_y == 6 and board_tiles[curr_x][curr_y - 2] is None:
+                avail_moves.append((curr_x, curr_y - 2))
+
+    def __get_en_passant_moves(self, board_tiles, curr_x, curr_y, prev_move):
+        avail_moves = []
+        if self.colour == TeamColour.WHITE:
+            if curr_y != 4:
+                return avail_moves
+            if
+
+            potential_moves = [(curr_x - 1, curr_y + 1),
+                (curr_x + 1, curr_y + 1)]
+            if
+        else:
+            potential_moves = [(curr_x - 1, curr_y - 1),
+                (curr_x + 1, curr_y - 1)]
+
+        if self.colour == TeamColour.WHITE:
+            if
--- test_chess_logic.py
+++ test_chess_logic.py
@@ -3,53 +3,210 @@ import unittest
 from chess_logic import *
 
 
-class BishopTests(unittest.TestCase):
+class ChessPieceTests(unittest.TestCase):
 
     def setUp(self):
         self.board = ChessBoard(layout='blank')
         self.board_tiles = self.board.get_tiles()
 
-    def test_get_moves_Bishop_Unblocked(self):
-        expected_moves =\
-            [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7)]
+    def add_piece(self, class_name, pos, colour):
+        piece_class = globals()[class_name]
+        piece = piece_class(pos, colour)
+        self.board_tiles[pos[0]][pos[1]] = piece
+        return piece
 
-        bishop = Bishop((0, 0), TeamColour.WHITE)
-        board_tiles = self.board.get_tiles()
-        board_tiles[0][0] = bishop
-        avail_moves = bishop.get_moves(self.board)
 
-        self.assertListEqual(expected_moves, avail_moves)
+class BishopTests(ChessPieceTests):
 
-    def test_get_moves_Bishop_BlockedSameColour(self):
-        expected_moves = [(1, 1), (2, 2), (3, 3)]
+    def test_get_moves_Unblocked1(self):
+        expected_moves = sorted(
+            [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7)])
 
-        bishop = Bishop((0, 0), TeamColour.WHITE)
-        self.board_tiles[0][0] = bishop
-        pawn = Pawn((4, 4), TeamColour.WHITE)
-        self.board_tiles[4][4] = pawn
-
-        avail_moves = bishop.get_moves(self.board)
+        piece = self.add_piece('Bishop', (0, 0), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
 
         self.assertListEqual(expected_moves, avail_moves)
 
-    def test_get_moves_Bishop_BlockedDifferentColour(self):
-        expected_moves = [(1, 1), (2, 2), (3, 3), (4, 4)]
+    def test_get_moves_Unblocked2(self):
+        expected_moves = sorted(
+            [(0, 0), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7),
+                (2, 0), (0, 2)])
 
-        bishop = Bishop((0, 0), TeamColour.WHITE)
-        self.board_tiles[0][0] = bishop
-        pawn = Pawn((4, 4), TeamColour.BLACK)
-        self.board_tiles[4][4] = pawn
+        piece = self.add_piece('Bishop', (1, 1), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
 
-        avail_moves = bishop.get_moves(self.board)
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_BlockedSameColour(self):
+        expected_moves = sorted([(1, 1), (2, 2), (3, 3)])
+
+        piece = self.add_piece('Bishop', (0, 0), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (4, 4), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_BlockedDifferentColour(self):
+        expected_moves = sorted([(1, 1), (2, 2), (3, 3), (4, 4)])
+
+        piece = self.add_piece('Bishop', (0, 0), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (4, 4), TeamColour.BLACK)
+        avail_moves = sorted(piece.get_moves(self.board))
 
         self.assertListEqual(expected_moves, avail_moves)
 
 
-# class ChessBoardTests(unittest.TestCase):
-#
-#     def test_init_BoardSetUpCorrectly(self):
-#         board = ChessBoard()
-#         print('hello, joe')
+class RookTests(ChessPieceTests):
+
+    def test_get_moves_Unblocked(self):
+        expected_moves = sorted(
+            [(4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7),
+                (0, 0), (1, 0), (2, 0), (3, 0), (5, 0), (6, 0), (7, 0)])
+
+        piece = self.add_piece('Rook', (4, 0), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_BlockedSameColour(self):
+        expected_moves = sorted(
+            [(4, 1), (4, 2), (4, 3),
+                (0, 0), (1, 0), (2, 0), (3, 0), (5, 0), (6, 0), (7, 0)])
+
+        piece = self.add_piece('Rook', (4, 0), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (4, 4), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_BlockedDifferentColour(self):
+        expected_moves = sorted(
+            [(4, 1), (4, 2), (4, 3), (4, 4),
+                (0, 0), (1, 0), (2, 0), (3, 0), (5, 0), (6, 0), (7, 0)])
+
+        piece = self.add_piece('Rook', (4, 0), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (4, 4), TeamColour.BLACK)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+
+class QueenTests(ChessPieceTests):
+
+    def test_get_moves_Unblocked(self):
+        expected_moves = sorted(
+            [(4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7),
+                (0, 0), (1, 0), (2, 0), (3, 0), (5, 0), (6, 0), (7, 0)])
+
+        piece = self.add_piece('Rook', (4, 0), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_BlockedSameColour(self):
+        expected_moves = sorted(
+            [(4, 1), (4, 2), (4, 3),
+                (0, 0), (1, 0), (2, 0), (3, 0), (5, 0), (6, 0), (7, 0)])
+
+        piece = self.add_piece('Rook', (4, 0), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (4, 4), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_BlockedDifferentColour(self):
+        expected_moves = sorted(
+            [(4, 1), (4, 2), (4, 3), (4, 4),
+                (0, 0), (1, 0), (2, 0), (3, 0), (5, 0), (6, 0), (7, 0)])
+
+        piece = self.add_piece('Rook', (4, 0), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (4, 4), TeamColour.BLACK)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+
+class KnightTests(ChessPieceTests):
+
+    def test_get_moves_Unblocked(self):
+        expected_moves = sorted(
+            [(1, 2), (1, 4), (2, 5), (4, 5), (5, 4), (5, 2), (4, 1), (2, 1)])
+
+        piece = self.add_piece('Knight', (3, 3), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_BlockedSameColour(self):
+        expected_moves = sorted([(2, 1)])
+
+        piece = self.add_piece('Knight', (0, 0), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (1, 2), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_BlockedDifferentColour(self):
+        expected_moves = sorted([(1, 2), (2, 1)])
+
+        piece = self.add_piece('Knight', (0, 0), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (1, 2), TeamColour.BLACK)
+        avail_moves = sorted(piece.get_moves(self.board))
+
+        self.assertListEqual(expected_moves, avail_moves)
+
+
+class PawnTests(ChessPieceTests):
+
+    def test_get_moves_White_CanTakeLeftOrRight(self):
+        expected_move_1 = (3, 2)
+        expected_move_2 = (5, 2)
+
+        piece = self.add_piece('Pawn', (4, 1), TeamColour.WHITE)
+        _ = self.add_piece('Rook', (3, 2), TeamColour.BLACK)
+        _ = self.add_piece('Rook', (5, 2), TeamColour.BLACK)
+        avail_moves = piece.get_moves(self.board)
+
+        self.assertIn(expected_move_1, avail_moves)
+        self.assertIn(expected_move_2, avail_moves)
+
+    def test_get_moves_Black_CanTakeLeftOrRight(self):
+        expected_move_1 = (3, 5)
+        expected_move_2 = (5, 5)
+
+        piece = self.add_piece('Pawn', (4, 6), TeamColour.BLACK)
+        _ = self.add_piece('Rook', (3, 5), TeamColour.WHITE)
+        _ = self.add_piece('Rook', (5, 5), TeamColour.WHITE)
+        avail_moves = piece.get_moves(self.board, prev_move=(-1, -1))
+
+        self.assertIn(expected_move_1, avail_moves)
+        self.assertIn(expected_move_2, avail_moves)
+
+    def test_get_moves_MoveForwardBlocked(self):
+        piece_1 = self.add_piece('Pawn', (4, 1), TeamColour.WHITE)
+        piece_2 = self.add_piece('Pawn', (6, 1), TeamColour.WHITE)
+
+        _ = self.add_piece('Rook', (4, 2), TeamColour.WHITE)
+        _ = self.add_piece('Rook', (6, 2), TeamColour.BLACK)
+
+        avail_moves_1 = piece_1.get_moves(self.board, prev_move=(-1, -1))
+        avail_moves_2 = piece_2.get_moves(self.board, prev_move=(-1, -1))
+
+        self.assertEqual(len(avail_moves_1), 0)
+        self.assertEqual(len(avail_moves_2), 0)
+
+    def test_get_moves_MoveForward(self):
+        expected_moves = sorted([(4, 2), (4, 3)])
+        piece = self.add_piece('Pawn', (4, 1), TeamColour.WHITE)
+        avail_moves = sorted(piece.get_moves(self.board, prev_move=(-1, -1)))
+        self.assertListEqual(expected_moves, avail_moves)
+
+    def test_get_moves_EnPassant(self):
+        expected_move = (3, 3)
+        piece = self.add_piece('Pawn', (4, 4), TeamColour.WHITE)
+        _ = self.add_piece('Pawn', (3, 4), TeamColour.BLACK)
+        avail_moves = piece.get_moves(self.board, prev_move=(3, 4))
+
 
 if __name__ == '__main__':
     unittest.main()
